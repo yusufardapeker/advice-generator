e8e68aca2a29fb749260c880350deb97fb71eb66
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-03-01 12:00:41 +0300

    Refactor script.js file to write more clean

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ const buttonELement = document.querySelector("button");
 const adviceID = document.querySelector(".advice-id");
 const adviceText = document.querySelector(".advice-text");
 
-// Self-Invoking Function
-(generateAdvice = () => {
-	fetch("https://api.adviceslip.com/advice")
-		.then((res) => res.json())
-		.then((data) => {
-			let id = data.slip.id;
-			let advice = data.slip.advice;
+const getData = async () => {
+	const res = await fetch("https://api.adviceslip.com/advice");
+	const data = await res.json();
 
-			adviceID.textContent = `ADVICE #${id}`;
-			adviceText.textContent = `"${advice}"`;
-		});
+	const {
+		slip: { id, advice },
+	} = data;
+
+	return { id, advice };
+};
+
+// Self-Invoking Function for first invoke
+(displayData = async () => {
+	const { id, advice } = await getData();
+
+	adviceID.textContent = `advice #${id}`;
+	adviceText.textContent = `"${advice}"`;
 })();
 
 alert("New advice request takes 2 seconds.");
 
-buttonELement.addEventListener("click", generateAdvice);
+buttonELement.addEventListener("click", displayData);
